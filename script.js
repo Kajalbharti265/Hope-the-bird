ad0ea2c35a842149426ec955c7f8e949ef476f2d
manan yadav <mananyadav1212@gmail.com>
2025-03-07 16:03:39 +0530

    changes

--- script.js
+++ script.js
@@ -31,5 +31,33 @@ document.addEventListener('keydown',(e) =>{
 function play(){
     function move(){
         if(game_state != 'Play') return;
+        let pipe_sprite = document.querySelector('pipe_sprite');
+        pipe_sprite.forEach((element) => {
+            let pipe_sprite_props = element.getBoundingClientRect();
+            bird_props = bird.getBoundingClientRect();
+
+            if(pipe_sprite_props.right <-+= 0){
+                element.remove();
+            }
+            else{
+                if(bird_props.left < pipe_sprite_props.left + pipe_sprite_props
+                    .width && bird_props.left + bird_props.width > pipe_sprite_props
+                .left && bird_props.top < pipe_sprite_props.top + pipe_sprite_props
+                .height && bird_props.top + bird_props.height > 
+                pipe_sprite_props.top){
+                    game_state = 'End';
+                    message.innerHTML = 'Game Over'.fontcolor('red') +
+                    '<br>Press Ebter To Restart';
+                    message.classList.add('messageStyle');
+                    imng.style.display = 'none';
+                    return;
+                }
+                else{
+                    if(pipe_sprite_props.right < bird_props.left &&
+                        pipe_sprite_props.right + move_speed >= bird_props.left
+                        && element.increase_score == '1')
+                }
+            }
+        })
     }
 }
